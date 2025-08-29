d08ad99de7efb4174b2ef3347efd948811943534
Sanju Singh <84117452+outlaw-xd@users.noreply.github.com>
2025-08-29 11:30:58 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -67,5 +67,5 @@ app.post("/bfhl", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
